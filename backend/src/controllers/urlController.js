3b9a9a8a1a1d089df75fa47f2d8d1537baf2e3ee
Ankit Sahu <ankit.sahu.as250@gmail.com>
2025-02-12 13:12:15 +0530

    ___

--- backend/src/controllers/urlController.js
+++ backend/src/controllers/urlController.js
@@ -113,9 +113,9 @@ export const getUrlAnalytics = async (req,res) =>{
         acc[date] = (acc[date]|| 0)+1;
         return acc;
        },{});
-       res.status(200).json({ unique_user, clicksByDate});
+       res.status(200).json({ unique_user, clicksByDate,uniqueOsCounts});
 
-      return res.status(200).json({unique_user,clicksByDate,uniqueOsCounts,uniqueDeviceCounts})
+      //return res.status(200).json({unique_user,clicksByDate,uniqueOsCounts,uniqueDeviceCounts})
       console.log('Sending response:fdsf', { unique_user, clicksByDate });
 
     }catch(Error){
